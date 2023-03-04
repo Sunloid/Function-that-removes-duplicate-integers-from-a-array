fc07848658cd7bd700dacd91b21462ec26229929
Syed ahmed haider <123230005+Sunloid@users.noreply.github.com>
2023-03-04 12:55:57 +0530

    Add files via upload

---
+++ Removing Duplicates from a array .js
@@ -0,0 +1,17 @@
+//Write a function that takes an array of integers and returns a new array with all the duplicates removed 
+var antiDuplicates = (arr1) =>{
+    var temp = arr1.length
+    for (var i=0;i<temp;i++){
+        for (var j=i+1;j<temp;j++){
+            if (arr1[i]==arr1[j]){
+                arr1.splice(i,1)
+            }
+            if (i == j ){
+                continue
+            }
+        }
+    }
+    return arr1
+}
+var res = antiDuplicates([5,5,9,10,11,5,5,6,7])
+console.log(res)
